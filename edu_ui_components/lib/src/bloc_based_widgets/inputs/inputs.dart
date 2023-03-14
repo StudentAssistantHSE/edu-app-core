5e09bcdc4b87f96ed58fd4641914ee38a81d884f
Kutilin Sergei <igameskut@icloud.com>
2023-03-14 21:25:09 +0300

    v0.0.1: fixed deps

--- edu_ui_components/lib/src/bloc_based_widgets/inputs/inputs.dart
+++ edu_ui_components/lib/src/bloc_based_widgets/inputs/inputs.dart
@@ -1,7 +1,7 @@
 export 'email_input_widget_settings.dart';
+export 'full_name_input_widget_settings.dart';
 export 'input_state_controller.dart';
 export 'input_widget.dart';
 export 'password_input_widget_settings.dart';
 export 'repeated_password_input_widget_settings.dart';
 export 'search_input_widget_settings.dart';
-export 'full_name_input_widget_settings.dart';
